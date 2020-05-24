2ccbbd78f7ebe0ec701504ae5e5df48ddb6cffbe
Maya D <65784949+M900-Pop@users.noreply.github.com>
2020-05-23 17:36:24 -0700

    Create moving mouth.py
    this is the least updated version of the moving mouth

---
+++ moving mouth.py
@@ -0,0 +1,55 @@
+import math
+import random
+import pygame
+
+def main():
+    pygame.init()
+    width = 500
+    height = 500
+    rows = 20
+    win = pygame.display.set_mode((width,height))
+    pygame.display.set_caption('Food Fall')
+    
+    x1 = 300
+    y1 = 300
+    
+    x1_change = 0       
+    y1_change = 0
+    
+    clock = pygame.time.Clock()
+    
+    game_over = False
+    while not game_over:    
+        for event in pygame.event.get():           
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -10
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = 10
+                    y1_change = 0
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    x1_change = 0
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = 0
+                    y1_change = 0
+        surf = pygame.Surface((width,height))
+        #surf.fill((0,255,0))
+        x1 += x1_change
+        y1 += y1_change
+        
+        center = (x1,y1)
+        radius = 40
+        pygame.draw.circle(surf, (0,0,255), center, radius)
+        win.blit(surf, (0,0))
+        pygame.display.update()
+        
+        clock.tick(30)
+
+    pygame.quit()
+    quit()
+main()
