fe02ac3852f5e6c698ee6c416cc9821152079fc1
seankusu <65787550+seankusu@users.noreply.github.com>
2020-05-23 17:57:43 -0700

    Update FoodFall.py
    New program (not with sushi png)

--- FoodFall.py
+++ FoodFall.py
@@ -5,10 +5,8 @@ pygame.init()
 win = pygame.display.set_mode((500,500))
 pygame.display.set_caption("Food Fall")
 
-count = 0
-speed = .2
 #this is mias change
-sushi1Img = pygame.image.load('sushi1.png')
+#sushi1Img = pygame.image.load('sushi1.png')
 
 
 def Sushi(x,y):
@@ -24,7 +22,7 @@ def dropSushi(x,speed, count):
         pygame.draw.rect(win, (0,0,0), (x, y , width, height))
         y+=speed
  
-def dropFood(x,speed, count):
+def dropFood(x,speed,color1, color2, color3):
     width = 40
     height = 60
     y = 0
@@ -34,24 +32,28 @@ def dropFood(x,speed, count):
         pygame.draw.rect(win, (0,0,0), (x, y , width, height))
         y+=speed
 
-         
+def main():
+    count = 0
+    speed = .2
+    
+    run = True
+    while run:
+        pygame.time.delay(100)
+        x = random.randint(0,460)
+        color1 = random.randint(0,255)
+        color2 = random.randint(0,255)
+        color3 = random.randint(0,255)
         
-run = True
-while run:
-    pygame.time.delay(100)
-    x = random.randint(0,460)
-    color1 = random.randint(0,255)
-    color2 = random.randint(0,255)
-    color3 = random.randint(0,255)
+        for event in pygame.event.get() :
+            if event.type == pygame.QUIT:
+                run = False
+                
+        dropFood(x, speed, color1, color2, color3)
+        count += 1
+        if count == 10:
+            speed += .1
     
-    for event in pygame.event.get() :
-        if event.type == pygame.QUIT:
-            run = False
-            
-    dropSushi(x, speed, count)
-    count += 1
-    if count == 10:
-        speed += .1
+    pygame.quit()
+    quit()
     
-pygame.quit()
-quit()
+main()
